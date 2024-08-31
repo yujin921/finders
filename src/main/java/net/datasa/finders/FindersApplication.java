f0953c63d7e6ecc27573936e2afe1a42810e0265
tayounglee <lahoo79@naver.com>
2024-08-31 15:02:32 +0900

    git first commit

---
+++ src/main/java/net/datasa/finders/FindersApplication.java
@@ -0,0 +1,13 @@
+package net.datasa.finders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindersApplication.class, args);
+    }
+
+}
