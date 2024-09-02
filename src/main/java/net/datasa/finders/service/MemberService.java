db2642043de625079909dd8c0c94f23a8c843659
hhsb2441 <hhsb2441@gmail.com>
2024-09-02 20:33:33 +0900

    회원가입 및 로그인 임시 기능 테스트 실행(구현 완료)

--- src/main/java/net/datasa/finders/service/MemberService.java
+++ src/main/java/net/datasa/finders/service/MemberService.java
@@ -1,30 +1,32 @@
 package net.datasa.finders.service;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import net.datasa.finders.domain.dto.MemberDTO;
 import net.datasa.finders.domain.entity.MemberEntity;
+import net.datasa.finders.domain.entity.RoleName;
 import net.datasa.finders.repository.MemberRepository;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
 public class MemberService {
 
+	private final BCryptPasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+    
     public void join(MemberDTO dto) {
-
-        private final BCryptPasswordEncoder passwordEncoder;
-
-        private final MemberRepository memberRepository;
 
         MemberEntity entity = MemberEntity.builder()
                 .memberId(dto.getMemberId())
                 .memberPw(passwordEncoder.encode(dto.getMemberPw()))        //비밀번호는 암호화
                 .memberName(dto.getMemberName())
                 .email(dto.getEmail())
-
+                .enabled(true) // 활성화 여부
+                .roleName(RoleName.ROLE_CLIENT) // 역할 설정
                 .build();
 
         memberRepository.save(entity);
