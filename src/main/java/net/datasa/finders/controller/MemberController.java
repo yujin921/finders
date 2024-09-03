f21401c28a8ea2ac349058aec8649490d3b482ef
feelkim97 <feelkim97@naver.com>
2024-09-03 19:55:23 +0900

    테스트 파일들 지웠어용!

--- src/main/java/net/datasa/finders/controller/MemberController.java
+++ src/main/java/net/datasa/finders/controller/MemberController.java
@@ -1,9 +1,21 @@
 package net.datasa.finders.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("member")
+
 public class MemberController {
 
-
+	@GetMapping("loginForm")
+	public String loginForm() {
+		return "member/loginForm";
+	}
 }
