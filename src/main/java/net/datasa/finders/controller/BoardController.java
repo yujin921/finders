f96f135fadb74a1fdaf3f0dc3239063c36350a57
tayounglee <lahoo79@naver.com>
2024-09-04 19:16:51 +0900

    BoardController내 컬럼명 수정 및 List 불러오는 기능 추가

--- src/main/java/net/datasa/finders/controller/BoardController.java
+++ src/main/java/net/datasa/finders/controller/BoardController.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * 거래 게시판 관련 콘트롤러
  */
@@ -31,7 +33,15 @@ public class BoardController {
     public String write() {
         return "board/writeForm";
     }
-	
+
+    @ResponseBody
+    @GetMapping("list")
+    public List<BoardDTO> list(@AuthenticationPrincipal AuthenticatedUser user) {
+        //서비스로 사용자 아이디를 전달하여 해당 아이디의 수입,지출 내역을 목록으로 리턴한다.
+        List<BoardDTO> list = boardService.getList(user.getUsername());
+        return list;
+    }
+
 	@PostMapping("write")
     public String write(
             @ModelAttribute BoardDTO boardDTO
@@ -44,12 +54,11 @@ public class BoardController {
         boardService.write(boardDTO);
         return "redirect:view";
     }
-	
-	
+
 	@GetMapping("read")
-	public String read(@RequestParam("boardNum") int boardNum, Model model, @AuthenticationPrincipal AuthenticatedUser user) {
+	public String read(@RequestParam("projectNum") int pNum, Model model, @AuthenticationPrincipal AuthenticatedUser user) {
 	    try {
-	        BoardDTO boardDTO = boardService.getBoard(boardNum);
+	        BoardDTO boardDTO = boardService.getBoard(pNum);
 	        model.addAttribute("board", boardDTO);
 	        model.addAttribute("user", user);
 	        return "board/read";
@@ -59,13 +68,13 @@ public class BoardController {
 	}
 	
 	@PostMapping("delete")
-    public String deletePost(@RequestParam("boardNum") int boardNum, @AuthenticationPrincipal AuthenticatedUser user) {
+    public String deletePost(@RequestParam("projectNum") int pNum, @AuthenticationPrincipal AuthenticatedUser user) {
         try {
-            BoardDTO boardDTO = boardService.getBoard(boardNum);
+            BoardDTO boardDTO = boardService.getBoard(pNum);
 
             // 로그인된 사용자와 게시글 작성자가 같은지 확인
             if (boardDTO.getClientId().equals(user.getUsername())) {
-                boardService.deleteBoard(boardNum);
+                boardService.deleteBoard(pNum);
                 return "redirect:/board/view"; // 게시글 목록 페이지로 리다이렉트
             } else {
                 return "redirect:/board/view?error=deletePermission"; // 권한 오류 페이지로 리다이렉트
