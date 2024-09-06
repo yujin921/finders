9477e6ca44867067160c0fc7546db65210551423
tayounglee <lahoo79@naver.com>
2024-09-06 15:07:53 +0900

    게시판 글쓰기 기능 작성중 ver 1

---
+++ src/main/java/net/datasa/finders/domain/entity/Board_SkillEntity.java
@@ -0,0 +1,27 @@
+package net.datasa.finders.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "project_required_skills")
+public class Board_SkillEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "project_num")
+    int projectNum;
+
+    @ManyToOne
+    @JoinColumn(name = "project_num", referencedColumnName = "project_num", insertable = false, updatable = false)
+    BoardEntity boardEntity;
+
+    @Column(name = "skill_text", nullable = false)
+    String skillText;
+}
