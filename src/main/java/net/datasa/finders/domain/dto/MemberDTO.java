23e3b5517aac01d5e48197a5fd0fbabd15d36234
feelkim97 <feelkim97@naver.com>
2024-09-07 18:56:22 +0900

    MemberDTO 에 dto통합 및 카카오 주소 찾기 기능 구현

--- src/main/java/net/datasa/finders/domain/dto/MemberDTO.java
+++ src/main/java/net/datasa/finders/domain/dto/MemberDTO.java
@@ -1,11 +1,13 @@
 package net.datasa.finders.domain.dto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import net.datasa.finders.domain.entity.MemberEntity;
 import net.datasa.finders.domain.entity.RoleName;
 
 @Builder
@@ -22,4 +24,16 @@ public class MemberDTO {
     RoleName roleName;
     LocalDateTime createdTime;
     LocalDateTime updatedTime;
+    // FreelancerDTO 내용
+    String freelancerId;
+	String freelancerPhone;
+	String country;
+	MemberEntity member;
+	// ClientDTO 내용
+	String clientId;
+	String clientPhone;
+	String industry;
+	LocalDate foundedDate;
+	Integer employeeCount;
+	String website;	
 }
