3fb10c5d199fc03c7cc5f1ee82059cfaa4137e9c
hhsb2441 <hhsb2441@gmail.com>
2024-09-07 19:09:04 +0900

    memberController 내용 추가 수정

--- src/main/java/net/datasa/finders/controller/MemberController.java
+++ src/main/java/net/datasa/finders/controller/MemberController.java
@@ -1,17 +1,5 @@
 package net.datasa.finders.controller;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.datasa.finders.domain.dto.ClientDTO;
@@ -19,6 +7,13 @@ import net.datasa.finders.domain.dto.FreelancerDTO;
 import net.datasa.finders.domain.dto.MemberDTO;
 import net.datasa.finders.domain.entity.MemberEntity;
 import net.datasa.finders.service.MemberService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @RequiredArgsConstructor
