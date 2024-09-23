af417413e6e55eb55ff2ca4fcd2e37dbc719e250
feelkim97 <feelkim97@naver.com>
2024-09-23 14:17:58 +0900

    마이페이지 오류 수정

--- src/main/java/net/datasa/finders/repository/FreelancerRepository.java
+++ src/main/java/net/datasa/finders/repository/FreelancerRepository.java
@@ -1,17 +1,16 @@
 package net.datasa.finders.repository;
 
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import net.datasa.finders.domain.entity.ClientEntity;
 import net.datasa.finders.domain.entity.FreelancerEntity;
 import net.datasa.finders.domain.entity.MemberEntity;
 
 @Repository
 
 public interface FreelancerRepository extends JpaRepository<FreelancerEntity, Integer> {
-	FreelancerEntity findByMember(MemberEntity member);
-
+	Optional<FreelancerEntity> findByMember(MemberEntity member);
 }
