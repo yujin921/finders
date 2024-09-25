84ee1a3fcbedbb1eb45d3453183d149a9fe73d94
gudwns0545 <gudwns0545@gmail.com>
2024-09-25 12:47:01 +0900

    프리랜서찾기 모든 필터 구현완료 검색창 미구현

--- src/main/java/net/datasa/finders/repository/ClientCategoryRepository.java
+++ src/main/java/net/datasa/finders/repository/ClientCategoryRepository.java
@@ -1,12 +1,17 @@
 package net.datasa.finders.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import net.datasa.finders.domain.entity.ClientCategoryEntity;
+import net.datasa.finders.domain.entity.ClientFieldEntity;
 import net.datasa.finders.domain.entity.MemberEntity;
 
 public interface ClientCategoryRepository extends JpaRepository<ClientCategoryEntity, Integer> {
 
 	void deleteByClientId(MemberEntity client);
+
+	Optional<ClientCategoryEntity> findByClientIdAndCategoryText(MemberEntity memberEntity, String area);
 
 }
--- src/main/java/net/datasa/finders/service/FindService.java
+++ src/main/java/net/datasa/finders/service/FindService.java
@@ -15,12 +15,14 @@ import net.datasa.finders.domain.entity.FreelancerReviewsEntity;
 import net.datasa.finders.domain.entity.FreelancerSkillEntity;
 import net.datasa.finders.domain.entity.MemberEntity;
 import net.datasa.finders.domain.entity.RoleName;
+import net.datasa.finders.domain.entity.TeamEntity;
 import net.datasa.finders.repository.ClientCategoryRepository;
 import net.datasa.finders.repository.ClientFieldRepository;
 import net.datasa.finders.repository.FreelancerPortfoliosRepository;
 import net.datasa.finders.repository.FreelancerReviewsRepository;
 import net.datasa.finders.repository.FreelancerSkillRepository;
 import net.datasa.finders.repository.MemberRepository;
+import net.datasa.finders.repository.TeamRepository;
 
 @Slf4j
 @Service
@@ -34,6 +36,7 @@ public class FindService {
 	private final FreelancerPortfoliosRepository freelancerPortfoliosRepository;
 	private final ClientFieldRepository clientFieldRepository;
 	private final ClientCategoryRepository clientCategoryRepository;
+	private final TeamRepository teamRepository;
 	
 	public List<FindFreelancerDTO> findFreelancerList(String[] fields, String[] areas) {
 		
@@ -49,42 +52,50 @@ public class FindService {
 			List<FreelancerReviewsEntity> freelancerReviewsEntityList = freelancerReviewsRepository.findByFreelancerId(memberEntity.getMemberId());
 			
 			for (String field : fields) {
-				if(clientFieldRepository.findByClientIdAndFieldText(memberEntity, field).isPresent()) {
-					for (FreelancerReviewsEntity freelancerReviewsEntity : freelancerReviewsEntityList) {
-						FreelancerReviewDTO freelancerReviewDTO = FreelancerReviewDTO.builder()
-								.rating(freelancerReviewsEntity.getRating())
+				for (String area : areas) {
+					if(clientFieldRepository.findByClientIdAndFieldText(memberEntity, field).isPresent() && clientCategoryRepository.findByClientIdAndCategoryText(memberEntity, area).isPresent()) {
+						for (FreelancerReviewsEntity freelancerReviewsEntity : freelancerReviewsEntityList) {
+							
+							FreelancerReviewDTO freelancerReviewDTO = FreelancerReviewDTO.builder()
+									.rating(freelancerReviewsEntity.getRating())
+									.build();
+							totalRating += freelancerReviewDTO.getRating();
+						} // 모든 평점 합산
+						
+						totalRating /= freelancerReviewsEntityList.size();
+						
+						if(Double.isNaN(totalRating)) {
+							totalRating = 0;
+						} // 평점이 없을 경우 0점 입력
+						
+						List<FreelancerSkillEntity> freelancerSkillEntityList = freelancerSkillRepository.findByFreelancerId(memberEntity);
+	
+						String[] skills = new String[freelancerSkillEntityList.size()];
+						int i = 0;
+						for (FreelancerSkillEntity freelancerSkillEntity : freelancerSkillEntityList) {
+							skills[i] = freelancerSkillEntity.getSkillText();
+							i += 1;
+						} // 프리랜서 보유 스킬 찾기
+						
+						List<FreelancerPortfoliosEntity> freelancerPortfoliosEntityList = freelancerPortfoliosRepository.findByMember(memberEntity);
+						
+						List<TeamEntity> teamEntityList = teamRepository.findByMember(memberEntity);
+						
+						FindFreelancerDTO findFreelancerDTO = FindFreelancerDTO.builder()
+								.memberId(memberEntity.getMemberId())
+								.profileImg(memberEntity.getProfileImg())
+								.totalRating(totalRating)
+								.totalPortfolios(freelancerPortfoliosEntityList.size())
+								.totalReviews(freelancerReviewsEntityList.size())
+								.totalProjects(teamEntityList.size())
+								.skills(skills)
 								.build();
-						totalRating += freelancerReviewDTO.getRating();
-					}
-					totalRating /= freelancerReviewsEntityList.size();
-					
-					if(Double.isNaN(totalRating)) {
-						totalRating = 0;
-					}
-					
-					List<FreelancerSkillEntity> freelancerSkillEntityList = freelancerSkillRepository.findByFreelancerId(memberEntity);
-
-					String[] skills = new String[freelancerSkillEntityList.size()];
-					int i = 0;
-					for (FreelancerSkillEntity freelancerSkillEntity : freelancerSkillEntityList) {
-						skills[i] = freelancerSkillEntity.getSkillText();
-						i += 1;
-					}
-					
-					List<FreelancerPortfoliosEntity> freelancerPortfoliosEntityList = freelancerPortfoliosRepository.findByMember(memberEntity);
-					
-					FindFreelancerDTO findFreelancerDTO = FindFreelancerDTO.builder()
-							.memberId(memberEntity.getMemberId())
-							.profileImg(memberEntity.getProfileImg())
-							.totalRating(totalRating)
-							.totalPortfolios(freelancerPortfoliosEntityList.size())
-							.totalReviews(freelancerReviewsEntityList.size())
-							.skills(skills)
-							.build();
-					if(!findFreelancerDTOList.contains(findFreelancerDTO)) {
-						findFreelancerDTOList.add(findFreelancerDTO);
-					}
-					
+						
+						if(!findFreelancerDTOList.contains(findFreelancerDTO)) {
+							findFreelancerDTOList.add(findFreelancerDTO);
+						} // 프리랜서 정보가 이미 들어가 있을 시 중복 제거를 위해 조건 추가
+						
+					};
 				};
 			};
 			
